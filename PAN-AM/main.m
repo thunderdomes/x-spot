b6cbe8601445e596dfaf8f7f92bc7dfaccd1058b
Arie <arie@wego.com>
2013-06-18 22:44:29 +0700

    Initial Commit

---
+++ PAN-AM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PAN-AM
+//
+//  Created by Arie on 6/18/13.
+//  Copyright (c) 2013 vootobox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "netraAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([netraAppDelegate class]));
+	}
+}
---
+++ PAN-AM/netraAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  netraAppDelegate.h
+//  PAN-AM
+//
+//  Created by Arie on 6/18/13.
+//  Copyright (c) 2013 vootobox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface netraAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
